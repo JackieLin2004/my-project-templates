6dbefbe747436d94887ddf197f64101d1cba3f2b
JackieLin2004 <linxy20040304@163.com>
2024-08-19 00:12:45 +0800

    Sending verification code vulnerability fixed

--- my-project-backend/src/main/java/com/example/config/RabbitConfiguration.java
+++ my-project-backend/src/main/java/com/example/config/RabbitConfiguration.java
@@ -2,6 +2,8 @@ package com.example.config;
 
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.core.QueueBuilder;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -13,6 +15,11 @@ public class RabbitConfiguration {
         return QueueBuilder
                 .durable("mail")
                 .build();
+    }
+
+    @Bean
+    public MessageConverter jsonMessageConverter() {
+        return new Jackson2JsonMessageConverter();
     }
 
 }
