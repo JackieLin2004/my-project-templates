0f062c79f2ccaca4c88e65c8ee794e332db5b07c
JackieLin2004 <linxy20040304@163.com>
2024-08-20 11:23:45 +0800

    Complete the verification of the email sending

--- my-project-backend/src/main/java/com/example/controller/AuthorizeController.java
+++ my-project-backend/src/main/java/com/example/controller/AuthorizeController.java
@@ -4,6 +4,9 @@ import com.example.entity.RestBean;
 import com.example.service.AccountService;
 import jakarta.annotation.Resource;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Pattern;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -12,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 /**
  * 用于验证相关Controller包含用户的注册、重置密码等操作
  */
+@Validated
 @RestController
 @RequestMapping("/api/auth")
 public class AuthorizeController {
@@ -28,8 +32,8 @@ public class AuthorizeController {
      * @return 是否请求成功
      */
     @GetMapping("/ask-code")
-    public RestBean<Void> askVerifyCode(@RequestParam String email,
-                                        @RequestParam String type,
+    public RestBean<Void> askVerifyCode(@RequestParam @Email String email,
+                                        @RequestParam @Pattern(regexp = "(register|reset)") String type,
                                         HttpServletRequest request) {
         String message = service.registerEmailVerifyCode(type, email, request.getRemoteAddr());
         return message == null ? RestBean.success() : RestBean.failure(400, message);
